7a7bf42a72a4489ce64de6269438b1fd55e5b799
IsrraDex36 <jonathanisrrael3609@gmail.com>
2022-09-30 18:43:23 -0500

    creacion del archivo php

---
+++ index.php
@@ -0,0 +1,358 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>CV personal</title>
+	<link rel="icon" href="Img/ico.ico">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+	rel="stylesheet">
+	<script src="js/bootstrap.bundle.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/animacion.css">
+
+</head>
+<body>
+	<div class="container-fluid">
+		<nav class="nav nav-pills fixed-top flex-column flex-sm-row">
+			<a class="flex-sm-fill text-sm-center nav-link active" aria-current="page" href="#Inicio">Sobre mi</a>
+			<a class="flex-sm-fill text-sm-center nav-link" href="#Estudios">Estudios</a>
+			<a class="flex-sm-fill text-sm-center nav-link" href="#Portafolio">Portafolio</a>
+			<a class="flex-sm-fill text-sm-center nav-link" href="#Intereses">Intereses</a>
+			<a class="flex-sm-fill text-sm-center nav-link" href="#Contacto">Contacto</a>
+		</nav>
+
+		<div class="row descrip" style=" background: black; color:white;">
+			<div class="col">
+				<br>
+				<br>
+				<h1 id="Inicio">JONATHAN ISRRAEL CABALLERO MORALES</h1>
+				<br>
+				<div class="text-center">
+					<img class="bd-placeholder-img rounded-circle" src="Img/P1.jpg" width="250" height="250">
+				</div>
+				<br>
+				<h3>CURRICULUM VITAE</h3>
+				<br><br><br>
+			</div>
+			<div class="col">
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<div class="Texto-anim">
+					<h3 class="h1-anim">SI PUEDES IMAGINARLO PUEDES PROGRAMARLO.<span>&#160;</span></h3>
+				</div>
+			</div>
+		</div>
+
+		<div class="row"  style=" background: #bbb; color:white;"">
+			<h1 id="Estudios">ESTUDIOS</h1>
+			<div class="col">
+				<div class="accordion" id="accordionExample">
+					<br>
+					<div class="accordion-item">
+						<h2 class="accordion-header" id="headingOne">
+							<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+								Secundaria
+							</button>
+						</h2>
+						<div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+							<div class="accordion-body">
+								<strong>ESCUELA SECUNDARIA GENERAL HERMANOS SERDAN.</strong> Cursados 3 años, al inicio presentaba poco interés por la escuela, pero con el paso del tiempo me dedique a ella. A partir del segundo año mi vida cambio y vi de otra manera la vida y los estudios, desde entonces he deacubierto mis fortalezas y debilidades.
+							</div>
+						</div>
+					</div>
+					<div class="accordion-item">
+						<h2 class="accordion-header" id="headingTwo">
+							<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+								Bachillerato
+							</button>
+						</h2>
+						<div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+							<div class="accordion-body">
+								<strong>CETIS N° 17 - TÉCNICO EN PROGRAMACIÓN.</strong> Cursados 3 años, 6 semestres, desarrollando mi lógica y razonamiento analítico en esos 3 años, durante la pandemia presentada el pasado 2019 entramos en cuarentena y pude estudiar más por mi cuenta y desarrollar habilidades que ni yo sabía que era capaz.							</div>
+						</div>
+					</div>
+					<div class="accordion-item">
+						<h2 class="accordion-header" id="headingThree">
+							<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+								TSU
+							</button>
+						</h2>
+						<div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+							<div class="accordion-body">
+								<strong>Actualmente en 3er Cuatrimestre de nivel TSU</strong> Ya cursados 2 cuatrimestres terminados con éxito y con calificación aceptable, Próximamente esperando llegar a Ingeniería. 
+							</div>
+						</div>
+					</div>
+					<div class="accordion-item">
+						<h2 class="accordion-header" id="headingFour">
+							<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseThree">
+								CURSOS
+							</button>
+						</h2>
+						<div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+							<div class="accordion-body">
+								<strong>Cursos a lo largo de mi pasión por la programación en general.</strong> 
+								<img src="Img/Cur-1.JPG">
+								<img src="Img/Cur-2.JPG">
+								<img src="Img/Cur-3.JPG">
+								<img src="Img/Cur-4.JPG">
+								<img src="Img/Cur-5.JPG">
+								<img src="Img/Cur-6.JPG">
+								<img src="Img/Cur-7.JPG">
+								<img src="Img/Cur-8.JPG">
+								<img src="Img/Cur-9.JPG">
+								<img src="Img/Cur-10.JPG">
+								<img src="Img/Cur-11.JPG">
+							</div>
+						</div>
+					</div>
+					<div class="accordion-item">
+						<h2 class="accordion-header" id="headingFive">
+							<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseThree">
+								Conocimientos y Habilidades
+							</button>
+						</h2>
+						<div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
+							<div class="accordion-body">
+								<strong>Estos son mis conocimientos y habilidades</strong>
+								<p>HTML</p>
+								<div class="progress">
+									<div class="progress-bar" role="progressbar" style="width: 95%;" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">95%</div>
+								</div>
+								<p>CSS</p>
+								<div class="progress">
+									<div class="progress-bar" role="progressbar" style="width: 70%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">70%</div>
+								</div>
+								<p>JAVA SCRIPT</p>
+								<div class="progress">
+									<div class="progress-bar" role="progressbar" style="width: 70%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">40%</div>
+								</div>
+								<p>JAVA</p>
+								<div class="progress">
+									<div class="progress-bar" role="progressbar" style="width: 60%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">60%</div>
+								</div>
+								<p>C++</p>
+								<div class="progress">
+									<div class="progress-bar" role="progressbar" style="width: 60%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">60%</div>
+								</div>
+								<p>PHYTON</p>
+								<div class="progress">
+									<div class="progress-bar" role="progressbar" style="width: 60%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">60%</div>
+								</div>
+								<p>C#</p>
+								<div class="progress">
+									<div class="progress-bar" role="progressbar" style="width: 30%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">30%</div>
+								</div>
+								<p>PHP</p>
+								<div class="progress">
+									<div class="progress-bar" role="progressbar" style="width: 35%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">35%</div>
+								</div>
+								<p>CREATIVIDAD</p>
+								<div class="progress">
+									<div class="progress-bar" role="progressbar" style="width: 99%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">99%</div>
+								</div>
+								<p>PENSAMIENTO ANALÍTICO</p>
+								<div class="progress">
+									<div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">90%</div>
+								</div>
+								<p>TRABAJO EN EQUIPO</p>
+								<div class="progress">
+									<div class="progress-bar" role="progressbar" style="width: 60%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">60%</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col">
+				<br>
+				<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img src="Img/E-1.jpg" class="d-block w-100" alt="...">
+						</div>
+						<div class="carousel-item">
+							<img src="Img/E-2.jpg" class="d-block w-100" alt="...">
+						</div>
+						<div class="carousel-item">
+							<img src="Img/E-3.jpg" class="d-block w-100" alt="...">
+						</div>
+					</div>
+					<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="visually-hidden">Previous</span>
+					</button>
+					<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="visually-hidden">Next</span>
+					</button>
+				</div>
+				<br><br><br><br><br>
+			</div>
+			<br><br><br><br><br>
+		</div>
+		
+			
+
+		<div class="row" style="background-color: #000;">
+			<h1 id="Portafolio" class="p1">PORTAFOLIO</h1>
+			<br><br><br>
+			<div class="col">
+				<div class="card mb-3" style="max-width: 540px;">
+					<div class="row g-0">
+						<div class="col-md-4">
+							<img src="Img/log.png" class="img-fluid rounded-start" alt="vida feliz">
+						</div>
+						<div class="col-md-8">
+							<div class="card-body">
+								<h5 class="card-title">Primer Cuatrimestre</h5>
+								<p class="card-text">En el Primer Cuatrimestre el proyecto a desarrollar fu  <b>TECNOLOGÍAS IDANA</b>, de acuerdo a lo pedido de cada materia expusimos pequeñas tareas que nos dejaron los profesores, cumpliendo en tiempo y forma con las actividades.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card mb-3" style="max-width: 540px;">
+					<div class="row g-0">
+						<div class="col-md-4">
+							<img src="Img/tec.jpg" class="img-fluid rounded-start" alt="vida feliz">
+						</div>
+						<div class="col-md-8">
+							<div class="card-body">
+								<h5 class="card-title">Segundo Cuatrimestre</h5>
+								<p class="card-text">En el Segundo Cuatrimestre el proyecto a desarrollar fue PC SPACE, Nuestro Proyecto está enfocada a resolver problemas tecnológicos en las PC, uno de ellos es esta página web la cual busca ayudar a vender un producto, en este caso computadoras, y a su vez dar ideas de reciclaje para evitar un daño al medio ambiente.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+				<div class="carousel-inner">
+					<div class="carousel-item active">
+						<img src="Img/Cua-1.jpg" class="d-block w-100" alt="...">
+					</div>
+					<div class="carousel-item">
+						<img src="Img/Cua-2.jpg" class="d-block w-100" alt="...">
+					</div>
+					<div class="carousel-item">
+						<img src="Img/Cua-3.JPG" class="d-block w-100" alt="...">
+					</div>
+				</div>
+				<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="visually-hidden">Previous</span>
+				</button>
+				<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="visually-hidden">Next</span>
+				</button>
+			</div>
+		</div>
+
+		<div class="h1-estudios">
+			<br><br>
+			
+
+			<br>
+			<div class="row" style=" background: #bbb;"">
+				<h1 id="Intereses" class="p2">INTERESES</h1>
+				<div class="col-sm-12 col-md-6 col-lg-3" >
+
+					<div class="card text-center" style="width: 18rem;">
+						<div class="card-body">
+							<h5 class="card-title"><span class="material-icons">draw</span></h5>
+							<h6 class="card-subtitle mb-2 text-muted">Dibujar</h6>
+							<p class="card-text">Me gusta dibujar ya sea caras reales o anime.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-3">
+					<div class="card text-center" style="width: 18rem;">
+						<div class="card-body">
+							<h5 class="card-title"><span class="material-icons">fitness_center</span>></h5>
+							<h6 class="card-subtitle mb-2 text-muted">Calistenía</h6>
+							<p class="card-text">Practico calistenia desde hace 2 años y hasta la fecha para mejorar.</p>
+						</div>
+					</div>
+				</div>
+				<br>
+				<div class="col-sm-12 col-md-6 col-lg-3">
+					<div class="card text-center" style="width: 18rem;">
+						<div class="card-body">
+							<h5 class="card-title"><span class="material-icons">book</span></h5>
+							<h6 class="card-subtitle mb-2 text-muted">Lectura</h6>
+							<p class="card-text">Me gusta leer desde hace 2 años. En especial ciencia ficción y Romance</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-3">
+					<div class="card text-center" style="width: 18rem;">
+						<div class="card-body">
+							<h5 class="card-title"><span class="material-icons">more_time</span></h5>
+							<h6 class="card-subtitle mb-2 text-muted">Aprovechar el tiempo</h6>
+							<p class="card-text">Me gusta aprovechar el tiempo, nunca descuido el tiempo.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row" style="background-color: #000;color: white;">
+			<div class="row text-center">
+				<h1 class="display-1" id="Contacto">Contáctanos</h1>
+			</div>
+
+			<br>
+
+			
+				<div class="col-md-4">
+					<h3>Redes sociales</h3>
+					<br>
+					<h3><a href="https://www.facebook.com/isrrael.morales.714"><ion-icon name="logo-facebook"></ion-icon></a></h3>
+					<h3><a href="https://www.instagram.com/isrrael_morales369/"><ion-icon name="logo-instagram"></ion-icon></a></h3>
+					<h3><a href="https://twitter.com/Jonatha27875406"><ion-icon name="logo-twitter"></ion-icon></a></h3>
+					<h3><a href="https://www.youtube.com/channel/UCO7SVd6xnLjkgAXWQ0jJOhA"><ion-icon name="logo-youtube"></ion-icon></a></h3>
+				</div>
+				<div class="col-md-4">
+					<h3>Información</h3>
+					<span class="material-icons">mail</span> jonathanisrrael3609@gmail.com <br>
+					<span class="material-icons">location_on</span> Tlahuapan,Ignacio Manuel Altamirano, Pue. <br>
+					<span class="material-icons">whatsapp</span> (+52) 2481536923<br>
+				</div>
+				<div class="col-md-4">
+					<h3>Ubicación en Google Maps</h3>
+					<div>
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7528.086925223202!2d-98.58678127660124!3d19.367270669427683!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1d602ca1845cd%3A0x89b74a2e46d65efd!2s74106%20Ignacio%20Manuel%20Altamirano%2C%20Pue.!5e0!3m2!1ses!2smx!4v1657765816245!5m2!1ses!2smx" width="450" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+					</div>
+				</div>
+			</div>
+
+		
+
+		
+			<divc class="row text-center" style=" background: #bbb; color:black;">
+				<div class="col-12">
+					<h4>DERECHOS RESERVADOS 2022</h4>
+				</div>
+			</divc>
+		
+
+
+	</div>
+
+	<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+	<script src="https://kit.fontawesome.com/3271d3d984.js" crossorigin="anonymous"></script>
+	
+</body>
+</html>
